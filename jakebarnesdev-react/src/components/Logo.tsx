4736ea47ad7de0a0d7d14b39a3ebc1516c4071bb
Jake Barnes <jakebarnescodes@gmail.com>
2026-09-22 22:20:12 -0500

    fixed mobile

--- jakebarnesdev-react/src/components/Logo.tsx
+++ jakebarnesdev-react/src/components/Logo.tsx
@@ -1,25 +1,10 @@
-import { useState } from 'react';
-import LogoImageJB from '../assets/logo_JAKEBARNES.png';
-import LogoImageD from '../assets/logo_DEV.png';
+import LogoImage from '../assets/logo.png';
 import './Logo.css'
 
 export function Logo() {
-    const [rotation, setRotation] = useState(0);
-
-    const addRotation = () => {
-        setRotation(rotation + 20);
-    }
-
     return (
-        <div className="jbd_logo">
-            <img className="jbd_logo_jb" src={LogoImageJB} alt="" />
-            <img
-                className="jbd_logo_d"
-                src={LogoImageD}
-                alt="" 
-                style={{ transform: `rotate(${rotation}deg)` }}
-                onClick={addRotation}
-            />
+        <div>
+            <img className="jbd_logo" src={LogoImage} alt="" />
         </div>
     );
 }
--- old-site/README.md
+++
@@ -1,1 +0,0 @@
-# jakebarnes-portfolio
